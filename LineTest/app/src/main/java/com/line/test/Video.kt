13f923d5f9a27a4155ff1d36d365d915efe2e1a3
Hung <hoangminhhung2531991@gmail.com>
2020-06-06 00:50:35 +0800

    - Read video from utils

---
+++ LineTest/app/src/main/java/com/line/test/Video.kt
@@ -0,0 +1,6 @@
+package com.line.test
+
+data class Video constructor(
+    val title: String,
+    val image: List<String>
+)
---
+++ LineTest/app/src/main/java/com/line/test/VideoUtils.kt
@@ -0,0 +1,29 @@
+package com.line.test
+
+import org.json.JSONObject
+
+object VideoUtils {
+
+    private val VIDEO_DATA: String = "{\n" +
+            "    \"title\" : \"Civil War\",\n" +
+            "    \"image\" : [\n" +
+            "        \"http://movie.phinf.naver.net/20151127_272/1448585271749MCMVs_JPEG/movie_image.jpg?type=m665_443_2\",\n" +
+            "        \"http://movie.phinf.naver.net/20151127_84/1448585272016tiBsF_JPEG/movie_image.jpg?type=m665_443_2\",\n" +
+            "        \"http://movie.phinf.naver.net/20151125_36/1448434523214fPmj0_JPEG/movie_image.jpg?type=m665_443_2\"\n" +
+            "    ]\n" +
+            "}"
+
+    fun readVideo(): Video {
+        val videoObject = JSONObject(VIDEO_DATA)
+        val title = videoObject.getString("title")
+        val imageObjects = videoObject.getJSONArray("image")
+        val images = arrayListOf<String>()
+        for (i in 0 until imageObjects.length()) {
+            val url = imageObjects.getString(i)
+            images.add(url)
+        }
+
+        return Video(title, images)
+    }
+
+}
